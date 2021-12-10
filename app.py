294956910e376e697070097fdd781d09366849c5
Konrad Jarodzki <korekjar@yahoo.pl>
2021-12-09 16:02:04 -0800

    LoopFix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import json
 from flask import Flask
 from flask import request, redirect
 from flask import render_template
@@ -12,11 +13,14 @@ data_dict = data[0]
 rates = data_dict.get('rates')
 keys=rates[0].keys()
 
+codes = []
+for data in rates:
+    codes.append(data.get('code'))
+print(codes)
+
 with open('output.csv', 'w', newline='', encoding="utf-8") as a_file:
     dict_writer = csv.DictWriter(a_file, keys, delimiter=';')
-    print("1")
     dict_writer.writeheader()
-    print("2")
     dict_writer.writerows(rates)
     a_file.close()
 
@@ -24,22 +28,18 @@ with open('output.csv', 'w', newline='', encoding="utf-8") as a_file:
 def calculate():
     if request.method == 'GET':
         print("We received GET")
-        return render_template("/calculator.html")
+        return render_template("/calculator.html", codes=codes)
     elif request.method == 'POST':
-        currency = request.form["currency"]
-        currency = str(currency)
+        data=request.form
+        currency = request.form['code']
+        amount = int(data.get('amount'))
         x=0
-        while currency != rates[x]["code"]:
-            x=x+1
-            return x
-        rate = rates[x]["bid"]
+        for data in rates:
+            if data.get('code') == currency:
+                rate = data.get('ask')
+                break
         print("rate", rate)
-        amount = request.form["amount"]
-        amount = float(amount)
         result = round(rate * amount,2)
         result = str(result)
         print("We received POST")
-        return render_template('/calculator.html', result=result)
-
-
-        
+        return render_template('/calculator.html', result=result, codes=codes)
